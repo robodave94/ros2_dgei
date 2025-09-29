2c947188737d324a30d98e3c66a9748f9b7d66bc
robodave94 <david.hinwood@canberra.edu.au>
2025-09-29 22:45:21 +1000

    setting up file

---
+++ dgei_gaze/scripts/dgei_gaze_node.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.parameter import Parameter
+from sensor_msgs.msg import Image
+from dgei_interfaces.msg import GazeFrame, GazeDetection
+
+from l2cs.gaze_detectors import Gaze_Detector
+
+import math
+import pdb
+import cv2
+import numpy as np
+from time import time, sleep
+from collections import deque
+
